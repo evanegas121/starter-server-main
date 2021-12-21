db9a95c2b07b0803cb19c08da86bc95c63cbd814
evanegas121 <Elizabeth.Vanegas11@myhunter.cuny.edu>
2021-12-20 23:08:23 -0500

    models

--- database/models/Campus.js
+++ database/models/Campus.js
@@ -3,27 +3,26 @@ const db = require('../db');
 
 const Campus = db.define("campus", {
 
-    // Do not allow Null, validate that the string is non-empty
+
     name: {
       type: Sequelize.STRING,
       allowNull: false,
       validate: {notEmpty: true}
     },
 
-    // Link to default icon for campuses
     imageURL: {
       type: Sequelize.STRING,
       defaultValue: "https://www.nicepng.com/png/detail/888-8880087_college-free-buildings-icons-flaticoncom-high-school-icon.png"
     },
 
-    // Address, also not Null or empty
+
     address: {
       type: Sequelize.STRING,
       allowNull: false,
       validate: {notEmpty: true}
     },
 
-    // Description, TEXT is unlimited size
+ 
     description: {
       type: Sequelize.TEXT
     }
