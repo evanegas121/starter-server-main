bfadb3843a5b50e09790718a350d35b661e0d9a9
evanegas121 <Elizabeth.Vanegas11@myhunter.cuny.edu>
2021-12-20 23:07:40 -0500

    models

--- database/models/Student.js
+++ database/models/Student.js
@@ -5,12 +5,29 @@ const Student = db.define("student", {
 
   firstname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {notEmpty: true}
   },
 
   lastname: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {notEmpty: true}
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {notEmpty: true}
+  },
+
+  imageURL: {
+    type: Sequelize.STRING,
+  },
+
+  gpa: {
+    type: Sequelize.DECIMAL(4),
+    validate: {max: 4, min: 0}
   }
 
 });
